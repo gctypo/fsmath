8a31d837987444c3e4558ad7c7b48c4e84227836
GCTypo <gctypo@outlook.com>
2023-07-18 01:07:41 -0700

    feat: add number parsing and basic structure

---
+++ fsmath.core/Tokenizer.fs
@@ -0,0 +1,50 @@
+﻿namespace fsmath.core
+
+open System.Text
+
+type TokenType =
+    | Number of whole: string * frac: string
+    | Operator of oper: string
+    | ParenOpen
+    | ParenClose
+
+type TokenizerResult =
+    | Complete of TokenType
+    | Partial of partial: string
+    | Empty
+
+module Tokenizer =
+
+    let toList (str: string) =
+        str.ToCharArray() |> Array.toList
+
+    let toString (lst: char list) =
+        let sb = (StringBuilder(), lst)
+                ||> List.fold (fun (s:StringBuilder) (c:char) -> s.Append c)
+        sb.ToString()
+
+    let isDigit (c: char) =
+        c >= '0' && c <= '9'
+
+    let rec tokenizeDigitSequence (feed: char list) (num: char list) =
+        match feed with
+        | c::tail when isDigit c ->
+            num @ [c] |> tokenizeDigitSequence tail
+        | _ -> (feed, num)
+
+    let rec parseNumber (feed: char list) (tok) =
+        match feed with
+        | c::tail when isDigit c ->
+            let (rem, n) = tokenizeDigitSequence tail [c]
+            parseNumber rem n
+        | '.'::c::tail when isDigit c ->
+            let (rem, n) = tokenizeDigitSequence tail [c]
+            (tok, n, rem)
+        | '.'::tail -> (tok, [], tail)
+        | _ -> (tok, [], feed)
+
+    let tokenizeNumber (feed: char list) =
+        let (w, p, rem) = parseNumber feed []
+        if (w = [] && p = []) then (rem, None)
+        else (rem, Number(toString w, toString p) |> Some)
+        //(rem, Number(toString w, toString p) |> Some)
---
+++ fsmath.test/TokenPart.Test.fs
@@ -0,0 +1,45 @@
+module fsmath.test
+
+open NUnit.Framework
+open FsUnit
+
+open fsmath.core
+open fsmath.core.Tokenizer
+
+[<Test>]
+[<TestCase('0', true)>]
+[<TestCase('9', true)>]
+[<TestCase('/', false)>]
+[<TestCase(':', false)>]
+let isDigit_Test (c: char, exp: bool) =
+    isDigit c
+    |> should equal exp
+
+[<Test>]
+[<TestCase("123", "123", "")>]
+[<TestCase("123+4", "123", "+4")>]
+[<TestCase("123.", "123", ".")>]
+[<TestCase("+4", "", "+4")>]
+let tokenizeDigitSequence_Test (inp: string, expNum: string, expRem: string) =
+    let (rem, num) = tokenizeDigitSequence (toList inp) []
+    num |> should equal expNum
+    rem |> should equal expRem
+
+[<Test>]
+[<TestCase("123", "123", "", "")>]
+[<TestCase("123.4", "123", "4", "")>]
+[<TestCase(".456", "", "456", "")>]
+[<TestCase("123.", "123", "", "")>]
+[<TestCase("+7", "", "", "+7")>]
+[<TestCase("123 4", "123", "", " 4")>]
+let tokenizeNumber_Test (inp: string, expWhole: string, expFrac: string, expRem: string) =
+    let (rem, num) = tokenizeNumber (toList inp)
+    if (expWhole = "" && expFrac = "") then
+        num |> should equal None
+        rem |> toString |> should equal expRem
+    else
+        match num.Value with
+        | Number(w, f) ->
+            (w, f) |> should equal (expWhole, expFrac)
+        | o -> Assert.Fail $"Wrong token {o}"
+        rem |> toString |> should equal expRem
